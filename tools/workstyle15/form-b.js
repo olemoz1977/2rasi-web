fac0cf3082293d8801c56396cd005d8c410a7cea
olemoz1977 <olemoz1977@gmail.com>
2026-08-23 14:51:58 +0300

    Add WorkStyle v0.4 form B checkpoint configuration

---
+++ tools/workstyle15/form-b.js
@@ -0,0 +1,7 @@
+(() => {
+  if (!window.WORKSTYLE15) throw new Error("WORKSTYLE15 data spec is missing before form-b configuration.");
+  window.WORKSTYLE15.instrument.formId = "workstyle15-lt-v0.4-form-b";
+  window.WORKSTYLE15.instrument.formLabel = "B";
+  window.WORKSTYLE15.instrument.sectionSize = 15;
+  window.WORKSTYLE15.instrument.sectionCount = 5;
+})();
