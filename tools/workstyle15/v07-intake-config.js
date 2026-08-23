f977d1c542baef7d16aa60ee430acd9425b645d4
olemoz1977 <olemoz1977@gmail.com>
2026-08-23 23:27:19 +0300

    chore(workstyle): prepare intake activation config

--- tools/workstyle15/v07-intake-config.js
+++ tools/workstyle15/v07-intake-config.js
@@ -3,5 +3,6 @@ window.WORKSTYLE_V07_INTAKE = Object.freeze({
   sessionUrl: '',
   storageKey: 'workstyle-v07-cognitive-session-e',
   expectedResponses: 34,
-  privacyText: 'Jei pasirinksi pateikti piloto duomenis, atsakymai, pažymėti neaiškumai ir atsakymo laikas bus perduoti tyrimo analizei. Vardo, el. pašto ar darbdavio neprašome.'
+  requestTimeoutMs: 15000,
+  privacyText: 'Jei pasirinksi pateikti piloto duomenis, atsakymai, pažymėti neaiškumai ir aktyvus atsakymo laikas bus perduoti tyrimo analizei. Vardo, el. pašto ar darbdavio neprašome.'
 });
