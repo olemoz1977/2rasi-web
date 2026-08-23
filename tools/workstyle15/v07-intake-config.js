3c9fff8a814f54c0a8e34ce38264b56d7bebbe43
olemoz1977 <olemoz1977@gmail.com>
2026-08-23 22:52:29 +0300

    chore(workstyle): add disabled pilot intake config

---
+++ tools/workstyle15/v07-intake-config.js
@@ -0,0 +1,7 @@
+window.WORKSTYLE_V07_INTAKE = Object.freeze({
+  enabled: false,
+  sessionUrl: '',
+  storageKey: 'workstyle-v07-cognitive-session-e',
+  expectedResponses: 34,
+  privacyText: 'Jei pasirinksi pateikti piloto duomenis, atsakymai, pažymėti neaiškumai ir atsakymo laikas bus perduoti tyrimo analizei. Vardo, el. pašto ar darbdavio neprašome.'
+});
