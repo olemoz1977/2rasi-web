42791f8c87c977205ab72b7e5cad59d9fbd30aef
olemoz1977 <olemoz1977@gmail.com>
2026-09-06 20:11:37 +0300

    fix(insights): recognize stable WorkStyle route

--- tools/site-insights.js
+++ tools/site-insights.js
@@ -22,6 +22,7 @@
       ['/tools/drama-triangle', 'drama-triangle'],
       ['/tools/strategic-thinking', 'strategic-thinking'],
       ['/tools/2pair', '2pair'],
+      ['/tools/workstyle', 'workstyle'],
       ['/tools/workstyle15', 'workstyle'],
       ['/tools/priolens', 'priolens'],
     ];
